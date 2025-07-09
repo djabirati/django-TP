bfab466486a06230fa717cb3d207d6316eb63f4a
Djabirati Marthadi <mgit config --global user.name Djabirati>
2025-07-09 14:34:52 +0200

    Ajout des routeurs

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,8 +1,23 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+
+router = DefaultRouter()
+router.register('feedback', views.FeedbackViewSet)
+
+"""
 urlpatterns = [
+    path('', include(router.urls)),
     path('<int:job_id>/', views.feedback_list, name='feedback_list'),
     path('add/', views.add_feedback, name='add_feedback'),
+]
+"""
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('feedback/<int:job_id>/', views.feedback_list, name='feedback_list'),
+    path('feedback/add/', views.add_feedback, name='add_feedback'),
 ]
--- job/urls.py
+++ job/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
+from .views import JobRecordViewSet
 
+router = DefaultRouter()
+router.register('jobs', JobRecordViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('stats/', views.stats_view, name='job-stats'),
     path('jobs/', views.job_list, name='job_list'),
     path('jobs/<int:job_id>/', views.job_detail, name='job_detail'),
